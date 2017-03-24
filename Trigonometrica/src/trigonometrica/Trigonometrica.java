fd9c0663aeec8c229499ce4e805a599036a70b13
Estudiantes <Estudiantes@labing50205.udistrital.edu.co>
2017-03-24 17:59:12 -0500

    Commit 3

---
+++ Trigonometrica/src/trigonometrica/Trigonometrica.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trigonometrica;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Trigonometrica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJFrame ventana = new NewJFrame ();
+        ventana.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
